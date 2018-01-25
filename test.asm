c2d11ad158126934578924268e560eb15401a2f3
Your Name <you@example.com>
2018-01-25 23:29:11 +0100

    Finalna verzija

--- test.asm
+++ test.asm
@@ -1,14 +1,17 @@
 %include "asm_io.inc"
 section .data
  v005 DD 0
+ v001 DD 0
 
 section .text
  global _start
 _start:
- MOV EAX,[v005]
- CALL print_int
- MOV EAX,1
- CALL print_int
+ MOV CX,5
+LOOPSTART1: 
+ PUSH CX
+ POP CX
+ DEC CX
+ JNZ LOOPSTART1
 INT 80h
 MOV EAX,1
 MOV EBX,0
